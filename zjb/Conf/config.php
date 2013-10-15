23fd8e4922a337cc5e870407d8c753bae4b60c98
Example Surname <845585849@qq.com>
2013-10-15 10:55:53 +0800

    room

--- zjb/Conf/config.php
+++ zjb/Conf/config.php
@@ -6,6 +6,13 @@ return array(
 		'SHOW_PAGE_TRACE' =>true, // 显示页面Trace信息
 	'USER_AUTH_KEY'=>'authId',
 	'SESSION_AUTO_START'    => true,
+  'DB_TYPE'               => 'mysql',     // 数据库类型
+    'DB_HOST'               => 'localhost', // 服务器地址
+    'DB_NAME'               => 'swg',          // 数据库名
+    'DB_USER'               => 'root',      // 用户名
+    'DB_PWD'                => '123',          // 密码
+    'DB_PORT'               => '3306',        // 端口
+    'DB_PREFIX'             => 'think_',    // 数据库表前缀
 
 );
 ?>
