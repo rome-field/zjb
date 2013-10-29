0a399a37e2eb9dfa542ca56a72e008d51d8afb51
rome-field <fieldrome@163.com>
2013-10-30 00:12:33 +0800

    admin on the way

--- zjb/Lib/Action/AdminAction.class.php
+++ zjb/Lib/Action/AdminAction.class.php
@@ -1,15 +1,40 @@
 <?php
 
-class AdminAction extends Action
-{
-    public function _initialize(){
-        if(!session('?admin_name')){
+class AdminAction extends Action {
+
+    public function _initialize() {
+        if (!session('?admin_name')) {
             $this->redirect('Adminlogin/login');
         }
     }
 
-    public function index(){
+    public function index() {
+        $db = M('user');
+        $total_mem = $db->count();
+        $this->assign('total_mem',$total_mem);
+        $common_mem = $db->where('user_type=0')->count();
+        $this->assign('common_mem',$common_mem);
+        $company_mem= $db->where('user_type=1')->count();
+        $this->assign('company_mem',$company_mem);
         $this->display();
     }
+
+    public function company_auth() {
+        $this->display();
+    }
+
+    public function info_auth() {
+        $this->display();
+    }
+
+    public function branch() {
+        $this->display();
+    }
+
+    public function logout() {
+        
+    }
+
 }
+
 ?>
